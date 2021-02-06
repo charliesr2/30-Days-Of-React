32536f95a8123a3b09ea5f1315e8034438b83fb5
Carlos Sánchez Romero <charliesr2@gmail.com>
2021-02-06 20:34:33 +0100

    Begins day0

---
+++ exercices/Day0/exercice1.js
@@ -0,0 +1,7 @@
+let emptyArray = [];
+let sixArray = Array(6);
+let sixArrayLength = sixArray.length;
+console.log(sixArrayLength);
+let mixedDataTypes = ['exe', 'png', 'jpg', 'mp3', 'dmg'];
+let mixedLength = mixedDataTypes.length;
+console.log(mixedLength);
